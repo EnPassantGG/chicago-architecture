68d0bb6bc8bcb6fbcaa90571ebae1103c0f7cbc0
Jacob <ll1bytecode@gmail.com>
2025-03-25 15:56:10 -0500

    Simplified the homepage by replacing Millennium Park details with a dynamic list of Chicago districts linking to dedicated pages. Created new templates and controller mappings for Downtown, North Side, West Side, South Side, and Suburbs.

--- src/main/java/com/example/chicagoarchitecture/controller/HomepageController.java
+++ src/main/java/com/example/chicagoarchitecture/controller/HomepageController.java
@@ -4,37 +4,55 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.LinkedHashMap;
 
 @Controller
 public class HomepageController {
 
-    // First page that users visiting the website will see
     @GetMapping("/")
     public String homePage(Model model) {
-        model.addAttribute("message", "Welcome to Chicago Architecture");
 
-        List<String> downtownMillenniumPark = Arrays.asList(
-                "Crown Fountain",
-                "Cloud Gate",
-                "Jay Pritzker Pavilion",
-                "Harris Theater for Music and Dance",
-                "McDonald's Cycle Center",
-                "Lurie Garden",
-                "Exelon Pavilions",
-                "Modern Wing at the Art Institute of Chicago",
-                "BP Pedestrian Bridge",
-                "Maggie Daley Park"
-                );
-        model.addAttribute("millenniumPark", downtownMillenniumPark);
+        // List of Chicago districts with respective webpages
+        LinkedHashMap<String, String> chicagoDistricts = new LinkedHashMap<>();
+        chicagoDistricts.put("Downtown", "downtown");
+        chicagoDistricts.put("North Side", "north_side");
+        chicagoDistricts.put("West Side", "west_side");
+        chicagoDistricts.put("South Side", "south_side");
+        chicagoDistricts.put("Suburbs", "suburbs");
+
+        model.addAttribute("chicagoDistricts", chicagoDistricts);
 
         return "homepage";
     }
 
-    // Link to a page containing additional details about the website and myself
     @GetMapping("/about")
     public String aboutPage() {
         return "about";
     }
+
+    @GetMapping("/downtown")
+    public String downtownDistrict() {
+        return "/chicagoDistricts/downtown";
+    }
+
+    @GetMapping("/north_side")
+    public String northSideDistrict() {
+        return "/chicagoDistricts/north_side";
+    }
+
+    @GetMapping("west_side")
+    public String westSideDistrict() {
+        return "/chicagoDistricts/west_side";
+    }
+
+    @GetMapping("south_side")
+    public String southSideDistrict() {
+        return "/chicagoDistricts/south_side";
+    }
+
+    @GetMapping("suburbs")
+    public String suburbDistrict() {
+        return "/chicagoDistricts/suburbs";
+    }
+
 }
