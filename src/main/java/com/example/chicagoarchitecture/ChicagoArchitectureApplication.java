2bf466c585bb7fe29e3db5a97ad2401b4487acce
Jacob <ll1bytecode@gmail.com>
2025-03-25 06:38:46 -0500

    Add initial Spring Boot project setup
    Include Maven configuration, basic application structure, and dependencies for Spring Boot, Thymeleaf, and Web.

---
+++ src/main/java/com/example/chicagoarchitecture/ChicagoArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.example.chicagoarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChicagoArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChicagoArchitectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chicagoarchitecture/ChicagoArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chicagoarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChicagoArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
