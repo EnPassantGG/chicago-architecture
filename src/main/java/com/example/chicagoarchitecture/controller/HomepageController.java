f21a6e6360b2a077ac207be1cfba2334036955b3
Jacob <ll1bytecode@gmail.com>
2025-03-25 07:56:10 -0500

    Add Millennium Park attractions to homepage.

--- src/main/java/com/example/chicagoarchitecture/controller/HomepageController.java
+++ src/main/java/com/example/chicagoarchitecture/controller/HomepageController.java
@@ -10,8 +10,9 @@ import java.util.List;
 @Controller
 public class HomepageController {
 
+    // First page that users see when they load the website
     @GetMapping("/")
-    public String homepage(Model model) {
+    public String homePage(Model model) {
         model.addAttribute("message", "Welcome to Chicago Architecture");
 
         List<String> downtownMillenniumPark = Arrays.asList(
@@ -29,5 +30,10 @@ public class HomepageController {
         model.addAttribute("millenniumPark", downtownMillenniumPark);
 
         return "homepage";
+    }
+
+    @GetMapping("/about")
+    public String aboutPage() {
+        return "about";
     }
 }
