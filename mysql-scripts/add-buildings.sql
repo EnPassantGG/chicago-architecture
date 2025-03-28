f5faf977dcd85918218a36f6c9ca6fb2ee4ccace
Jacob <ll1bytecode@gmail.com>
2025-03-27 20:20:47 -0500

    Consolidated multiple individual `INSERT` statements into batch inserts for improved brevity and readability.

--- mysql-scripts/add-buildings.sql
+++ mysql-scripts/add-buildings.sql
@@ -1,66 +1,43 @@
 -- Suburbs: North Suburbs
 INSERT INTO chicagoarchitecture.building (name, address, city, creation_year, district)
-VALUES ("Shure Corporate Headquarters", "5800 West Touhy Avenue", "Niles", 2004, "north_suburbs");
-INSERT INTO chicagoarchitecture.building (name, address, city, creation_year, district)
-VALUES ("Levy Senior Center", "300 Dodge Avenue", "Evanston", 2002, "north_suburbs");
-INSERT INTO chicagoarchitecture.building (name, address, city, creation_year, district)
-VALUES ("Jewish Reconstructionist Congregation", "303 Dodge Avenue", "Evanston", 2008, "north_suburbs");
-INSERT INTO chicagoarchitecture.building (name, address, city, creation_year, district)
-VALUES ("Optima Horizons", "800 Elgin Road", "Evanston", 2004, "north_suburbs");
-INSERT INTO chicagoarchitecture.building (name, address, city, creation_year, district)
-VALUES ("Segal Visitors Center", "1841 Sheridan Road", "Evanston", 2014, "north_suburbs");
-
-INSERT INTO chicagoarchitecture.building (name, address, city, creation_year, district)
-VALUES ("Ryan Center for the Musical Arts", "70 Arts Circle Drive", "Evanston", 2015, "north_suburbs");
-INSERT INTO chicagoarchitecture.building (name, address, city, creation_year, district)
-VALUES ("Kellogg School of Management Global Hub", "2211 Campus Drive", "Evanston", 2017, "north_suburbs");
-INSERT INTO chicagoarchitecture.building (name, address, city, creation_year, district)
-VALUES ("Optima Old Orchard Woods", "9739 Woods Drive", "Skokie", 2008, "north_suburbs");
-INSERT INTO chicagoarchitecture.building (name, address, city, creation_year, district)
-VALUES ("Illinois Holocaust Museum & Education Center", "9603 Woods Drive", "Skokie", 2009, "north_suburbs");
-INSERT INTO chicagoarchitecture.building (name, address, city, creation_year, district)
-VALUES ("Lipton Thayer House", "2600 Thayer Street", "Evanston", 2018, "north_suburbs");
-
-INSERT INTO chicagoarchitecture.building (name, address, city, creation_year, district)
-VALUES ("Wilmette Residence", "1019 Linden Avenue", "Wilmette", 2016, "north_suburbs");
-INSERT INTO chicagoarchitecture.building (name, address, city, creation_year, district)
-VALUES ("Glass House & 7RR Eco-Home", "Rolling Ridge Lane", "Northfield", 2012, "north_suburbs");
-INSERT INTO chicagoarchitecture.building (name, address, city, creation_year, district)
-VALUES ("Crate & Barrel Headquarters", "1250 Techny Road", "Northbrook", 2002, "north_suburbs");
-INSERT INTO chicagoarchitecture.building (name, address, city, creation_year, district)
-VALUES ("Writers Theatre", "325 Tudor Court", "Glencoe", 2016, "north_suburbs");
-INSERT INTO chicagoarchitecture.building (name, address, city, creation_year, district)
-VALUES ("Daniel F. and Ada L. Rice Plant Science Conservation Center", "1000 Lake Cook Road", "Glencoe", 2009, "north_suburbs");
-
-INSERT INTO chicagoarchitecture.building (name, address, city, creation_year, district)
-VALUES ("Ravinia Festival Dining Pavilion", "418 Sheridan Road", "Glencoe", 2007, "north_suburbs");
-INSERT INTO chicagoarchitecture.building (name, address, city, creation_year, district)
-VALUES ("Ragdale Meadow Studio", "1260 Green Bay Road", "Lake Forest", 2008, "north_suburbs");
+VALUES 
+    ("Shure Corporate Headquarters", "5800 West Touhy Avenue", "Niles", 2004, "north_suburbs"),
+    ("Levy Senior Center", "300 Dodge Avenue", "Evanston", 2002, "north_suburbs"),
+    ("Jewish Reconstructionist Congregation", "303 Dodge Avenue", "Evanston", 2008, "north_suburbs"),
+    ("Optima Horizons", "800 Elgin Road", "Evanston", 2004, "north_suburbs"),
+    ("Segal Visitors Center", "1841 Sheridan Road", "Evanston", 2014, "north_suburbs"),
+    ("Ryan Center for the Musical Arts", "70 Arts Circle Drive", "Evanston", 2015, "north_suburbs"),
+    ("Kellogg School of Management Global Hub", "2211 Campus Drive", "Evanston", 2017, "north_suburbs"),
+    ("Optima Old Orchard Woods", "9739 Woods Drive", "Skokie", 2008, "north_suburbs"),
+    ("Illinois Holocaust Museum & Education Center", "9603 Woods Drive", "Skokie", 2009, "north_suburbs"),
+    ("Lipton Thayer House", "2600 Thayer Street", "Evanston", 2018, "north_suburbs"),
+    ("Wilmette Residence", "1019 Linden Avenue", "Wilmette", 2016, "north_suburbs"),
+    ("Glass House & 7RR Eco-Home", "Rolling Ridge Lane", "Northfield", 2012, "north_suburbs"),
+    ("Crate & Barrel Headquarters", "1250 Techny Road", "Northbrook", 2002, "north_suburbs"),
+    ("Writers Theatre", "325 Tudor Court", "Glencoe", 2016, "north_suburbs"),
+    ("Daniel F. and Ada L. Rice Plant Science Conservation Center", "1000 Lake Cook Road", "Glencoe", 2009, "north_suburbs"),
+    ("Ravinia Festival Dining Pavilion", "418 Sheridan Road", "Glencoe", 2007, "north_suburbs"),
+    ("Ragdale Meadow Studio", "1260 Green Bay Road", "Lake Forest", 2008, "north_suburbs");
 
 
 -- Suburbs: Northwest and West Suburbs
 INSERT INTO chicagoarchitecture.building (name, address, city, creation_year, district)
-VALUES ("Technical Education Center, College of Dupage", "425 22nd Street", "Glen Ellyn", 2010, "northwest_and_west_suburbs");
-INSERT INTO chicagoarchitecture.building (name, address, city, creation_year, district)
-VALUES ("Beem Building, College of Dupage", "425 Fawell Boulevard", "Glen Ellyn", 2007, "northwest_and_west_suburbs");
-INSERT INTO chicagoarchitecture.building (name, address, city, creation_year, district)
-VALUES ("Morton Arboretum Visitor Center", "4100 Illinois Route 53", "Lisle", 2004, "northwest_and_west_suburbs");
-INSERT INTO chicagoarchitecture.building (name, address, city, creation_year, district)
-VALUES ("Dupage Children's Museum", "301 North Washington Street", "Naperville", 2002, "northwest_and_west_suburbs");
-INSERT INTO chicagoarchitecture.building (name, address, city, creation_year, district)
-VALUES ("Serta International", "2600 Forbs Avenue", "Hoffman Estates", 2009, "northwest_and_west_suburbs");
-
-INSERT INTO chicagoarchitecture.building (name, address, city, creation_year, district)
-VALUES ("Zurich North America", "190 and Meacham Road", "Schaumburg", 2016, "northwest_and_west_suburbs");
-INSERT INTO chicagoarchitecture.building (name, address, city, creation_year, district)
-VALUES ("Trumpf Smart Factory", "1900 West Central Road", "Hoffman Estates", 2009, "northwest_and_west_suburbs");
+VALUES 
+    ("Technical Education Center, College of Dupage", "425 22nd Street", "Glen Ellyn", 2010, "northwest_and_west_suburbs"),
+    ("Beem Building, College of Dupage", "425 Fawell Boulevard", "Glen Ellyn", 2007, "northwest_and_west_suburbs"),
+    ("Morton Arboretum Visitor Center", "4100 Illinois Route 53", "Lisle", 2004, "northwest_and_west_suburbs"),
+    ("Dupage Children's Museum", "301 North Washington Street", "Naperville", 2002, "northwest_and_west_suburbs"),
+    ("Serta International", "2600 Forbs Avenue", "Hoffman Estates", 2009, "northwest_and_west_suburbs"),
+    ("Zurich North America", "190 and Meacham Road", "Schaumburg", 2016, "northwest_and_west_suburbs"),
+    ("Trumpf Smart Factory", "1900 West Central Road", "Hoffman Estates", 2009, "northwest_and_west_suburbs");
 
 
 -- Personal Reccomendations
 INSERT INTO chicagoarchitecture.building (name, address, city, creation_year, district, is_endorsed)
-VALUES ("Lee Street Beach", "1111 Lake Shore Blvd", "Evanston", 1200000000, "north_suburbs", TRUE);
-INSERT INTO chicagoarchitecture.building (name, address, city, creation_year, district, is_endorsed)
-VALUES ("Northwestern University Rebecca Crown Center", "633 Clark St", "Evanston", 1968, "north_suburbs", TRUE);
+VALUES 
+    ("Lee Street Beach", "1111 Lake Shore Blvd", "Evanston", 1200000000, "north_suburbs", TRUE),
+    ("Northwestern University Rebecca Crown Center", "633 Clark St", "Evanston", 1968, "north_suburbs", TRUE);
+
 
 
 -- Show everything that was added
