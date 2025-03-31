db611baa83d87bc8474f8e9abcbee467d25b2a62
Jacob <ll1bytecode@gmail.com>
2025-03-31 15:43:25 -0500

    Simplify and reorganize SQL scripts for MySQL and AWS.

---
+++ mysql-scripts/3. create-tables-aws.sql
@@ -0,0 +1,18 @@
+-- These lines were added to get AWS to work correctly
+-- (Consider making this a seperate SQL script)
+CREATE DATABASE IF NOT EXISTS `architecture_directory`;
+USE `architecture_directory`;
+
+DROP TABLE IF EXISTS building; -- correct spelling
+
+CREATE TABLE IF NOT EXISTS building (
+	id INT AUTO_INCREMENT PRIMARY key,
+    name VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    creation_year INT NOT NULL,
+    district VARCHAR(255) NOT NULL,
+    image_path VARCHAR(255) DEFAULT NULL,
+    is_endorsed BOOLEAN DEFAULT FALSE
+)
+
---
+++ mysql-scripts/4. add-buildings-aws.sql
@@ -0,0 +1,118 @@
+USE `architecture_directory`;
+
+-- Downtown: Millennium Park
+INSERT INTO building (name, address, city, creation_year, district, image_path)
+VALUES 
+    ("Crown Fountain", "Michigan Avenue", "Chicago", "2004", "millennium_park", "/images/millenniumPark/crown-fountain.jpg"),
+    ("Cloud Gate", "Michigan Avenue", "Chicago", "2006", "millennium_park", "/images/millenniumPark/cloud-gate.jpeg"),
+    ("Jay Pritzker Pavilion", "201 East Randolph Street", "Chicago", "2004", "millennium_park", "/images/millenniumPark/jay-pritzker-pavilion.jpg"),
+    ("Harris Theater for Music and Dance", "205 East Randolph Street", "Chicago", "2003", "millennium_park", NULL),
+    ("McDonald's Cycle Center", "239 East Randolph Street", "Chicago", "2004", "millennium_park", NULL),
+    ("Lurie Garden", "Monroe Street", "Chicago", "2004", "millennium_park", NULL),
+    ("Excelon Pavilions", "Randolph Street and Monroe Street", "Chicago", "2004", "millennium_park", NULL),
+    ("Modern Wing at the Art Intitute of Chicago", "Monroe Street", "Chicago", "2003", "millennium_park", NULL),
+    ("BP Pedestrian Bridge", "Columbus Drive", "Chicago", "2004", "millennium_park", NULL),
+    ("Maggie Daley Park", "337 East Randolph Street", "Chicago", "2006", "millennium_park", NULL);
+    
+    
+-- Downtown: Lakeshore East
+INSERT INTO building (name, address, city, creation_year, district, image_path)
+VALUES 
+    ("Blue Cross-Blue Shield Tower", "300 East Randolph Street", "Chicago", "2010", "lakeshore_east", NULL),
+    ("340 on the Park", "340 East Randolph Street", "Chicago", "2007", "lakeshore_east", NULL),
+    ("The Park at Lakeshore East", "405 East Benton Place", "Chicago", "2005", "lakeshore_east", NULL),
+    ("Aqua Tower", "225 North Columbus Drive", "Chicago", "2010", "lakeshore_east", NULL),
+    ("Gems World Academy", "350 East South Water Street", "Chicago", "2020", "lakeshore_east", NULL),
+    ("Coast at Lakeshore East", "345 East Wacker Drive", "Chicago", "2013", "lakeshore_east", NULL),
+    ("St. Regis Chicago", "363 East Wacker Drive", "Chicago", "2020", "lakeshore_east", NULL);
+    
+
+-- Downtown: The Loop (East)
+INSERT INTO building (name, address, city, creation_year, district, image_path)
+VALUES 
+    ("Chicago Architecture Center", "111 East Wacker Drive", "Chicago", "2018", "the_loop_east", NULL),
+    ("Chicago Riverwalk", "Chicago River", "Chicago", "2016", "the_loop_east", NULL),
+    ("The WIT", "201 North State Street", "Chicago", "2009", "the_loop_east", NULL),
+    ("Block 37", "108 North State Street", "Chicago", "2016", "the_loop_east", NULL),
+    ("CTA Washington/Wabash Station", "Wabash Avenue at Washington Street", "Chicago", "2010", "the_loop_east", NULL),
+    ("The Legacy at Millennium Park", "60 East Monroe Street", "Chicago", "2010", "the_loop_east", NULL),
+    ("11 West Quincy Court Federal Building", "11 West Quincy Street", "Chicago", "2012", "the_loop_east", NULL),
+    ("Rossevelt University Academic, Student Life and Residence Center", "450 South Wabash Avenue", "Chicago", "2012", "the_loop_east", NULL);
+    
+    
+-- Downtown: The Loop (West)
+INSERT INTO building (name, address, city, creation_year, district, image_path)
+VALUES 
+    ("One South Dearborn", "1 South Dearborn Street", "Chicago", "2005", "the_loop_west", NULL),
+    ("235 Van Buren", "235 Van Buren Street", "Chicago", "2009", "the_loop_west", NULL),
+    ("Hyatt Center", "71 South Wacker Drive", "Chicago", "2005", "the_loop_west", NULL),
+    ("UBS Tower", "1 North Wacker Drive", "Chicago", "2001", "the_loop_west", NULL),
+    ("111 South Wacker", "111 South Wacker Drive", "Chicago", "2005", "the_loop_west", NULL),
+    ("155 North Wacker", "155 North Wacker Drive", "Chicago", "2009", "the_loop_west", NULL),
+    ("CNA Center", "151 North Franklin Street", "Chicago", "2018", "the_loop_west", NULL),
+    ("OneEleven", "111 West Wacker Drive", "Chicago", "2014", "the_loop_west", NULL),
+    ("Riverbend", "333 North Canal Street", "Chicago", "2002", "the_loop_west", NULL),
+    ("River Point", "444 West Lake Street", "Chicago", "2017", "the_loop_west", NULL),
+    ("150 North Riverside", "150 North Riverside Plaza", "Chicago", "2018", "the_loop_west", NULL);
+    
+    
+-- Downtown: South Loop & Chinatown
+INSERT INTO building (name, address, city, creation_year, district, image_path)
+VALUES 
+    ("Cook County Circuit Courthouse", "555 West Harrison Street", "Chicago", "2005", "south_loop_and_chinatown", NULL),
+    ("William Jones College Preparatory High School", "700 South State Street", "Chicago", "2013", "south_loop_and_chinatown", NULL),
+    ("Spertus Intitute", "610 South Michigan Avenue", "Chicago", "2007", "south_loop_and_chinatown", NULL),
+    ("618 South Michigan Avenue Facade", "618 South Michigan Avenue", "Chicago", "2012", "south_loop_and_chinatown", NULL),
+    ("Student Life Center for East-West University", "829 South Wabash Avenue", "Chicago", "2013", "south_loop_and_chinatown", NULL),
+    ("Nema Chicago", "1200 South Indiana Avenue", "Chicago", "2019", "south_loop_and_chinatown", NULL),
+    ("Soldier Field and North Burnham Park Redevelopment", "1410 Museum Campus Drive", "Chicago", "2003", "south_loop_and_chinatown", NULL),
+    ("Northerly Island", "1521 South Linn White Drive", "Chicago", "2015", "south_loop_and_chinatown", NULL),
+    ("Columbia College Chicago Media Production Center", "1600 South State Street", "Chicago", "2010", "south_loop_and_chinatown", NULL),
+    ("Perspective Charter School Rodney D. Joslin Campus", "1930 South Archer Avenue", "Chicago", "2004", "south_loop_and_chinatown", NULL),
+    ("CTA Cermak-McCormick Place Station", "12 East Cermak Road", "Chicago", "2015", "south_loop_and_chinatown", NULL),
+    ("McCormick Place West", "Cermak Road", "Chicago", "2007", "south_loop_and_chinatown", NULL),
+    ("Ping Tom Memorial Park Boathouse & Fieldhouse", "300 West 19th Street", "Chicago", "2013", "south_loop_and_chinatown", NULL),
+    ("Chinese American Service League Kam L. Liu Building", "2141 South Tan Court", "Chicago", "2004", "south_loop_and_chinatown", NULL),
+    ("Chinatown Branch Library", "2100 South Wentworth Avenue", "Chicago", "2015", "south_loop_and_chinatown", NULL);
+    
+-- Suburbs: North Suburbs
+INSERT INTO building (name, address, city, creation_year, district)
+VALUES 
+    ("Shure Corporate Headquarters", "5800 West Touhy Avenue", "Niles", 2004, "north_suburbs"),
+    ("Levy Senior Center", "300 Dodge Avenue", "Evanston", 2002, "north_suburbs"),
+    ("Jewish Reconstructionist Congregation", "303 Dodge Avenue", "Evanston", 2008, "north_suburbs"),
+    ("Optima Horizons", "800 Elgin Road", "Evanston", 2004, "north_suburbs"),
+    ("Segal Visitors Center", "1841 Sheridan Road", "Evanston", 2014, "north_suburbs"),
+    ("Ryan Center for the Musical Arts", "70 Arts Circle Drive", "Evanston", 2015, "north_suburbs"),
+    ("Kellogg School of Management Global Hub", "2211 Campus Drive", "Evanston", 2017, "north_suburbs"),
+    ("Optima Old Orchard Woods", "9739 Woods Drive", "Skokie", 2008, "north_suburbs"),
+    ("Illinois Holocaust Museum & Education Center", "9603 Woods Drive", "Skokie", 2009, "north_suburbs"),
+    ("Lipton Thayer House", "2600 Thayer Street", "Evanston", 2018, "north_suburbs"),
+    ("Wilmette Residence", "1019 Linden Avenue", "Wilmette", 2016, "north_suburbs"),
+    ("Glass House & 7RR Eco-Home", "Rolling Ridge Lane", "Northfield", 2012, "north_suburbs"),
+    ("Crate & Barrel Headquarters", "1250 Techny Road", "Northbrook", 2002, "north_suburbs"),
+    ("Writers Theatre", "325 Tudor Court", "Glencoe", 2016, "north_suburbs"),
+    ("Daniel F. and Ada L. Rice Plant Science Conservation Center", "1000 Lake Cook Road", "Glencoe", 2009, "north_suburbs"),
+    ("Ravinia Festival Dining Pavilion", "418 Sheridan Road", "Glencoe", 2007, "north_suburbs"),
+    ("Ragdale Meadow Studio", "1260 Green Bay Road", "Lake Forest", 2008, "north_suburbs");
+
+
+-- Suburbs: Northwest and West Suburbs
+INSERT INTO building (name, address, city, creation_year, district)
+VALUES 
+    ("Technical Education Center, College of Dupage", "425 22nd Street", "Glen Ellyn", 2010, "northwest_and_west_suburbs"),
+    ("Beem Building, College of Dupage", "425 Fawell Boulevard", "Glen Ellyn", 2007, "northwest_and_west_suburbs"),
+    ("Morton Arboretum Visitor Center", "4100 Illinois Route 53", "Lisle", 2004, "northwest_and_west_suburbs"),
+    ("Dupage Children's Museum", "301 North Washington Street", "Naperville", 2002, "northwest_and_west_suburbs"),
+    ("Serta International", "2600 Forbs Avenue", "Hoffman Estates", 2009, "northwest_and_west_suburbs"),
+    ("Zurich North America", "190 and Meacham Road", "Schaumburg", 2016, "northwest_and_west_suburbs"),
+    ("Trumpf Smart Factory", "1900 West Central Road", "Hoffman Estates", 2009, "northwest_and_west_suburbs");
+
+
+-- Personal Reccomendations
+INSERT INTO building (name, address, city, creation_year, district, image_path, is_endorsed)
+VALUES 
+    ("Lee Street Beach", "1111 Lake Shore Blvd", "Evanston", 1200000000, "north_suburbs", "/images/reccomendations/lee-street-beach.jpg", TRUE),
+    ("Northwestern University Rebecca Crown Center", "633 Clark St", "Evanston", 1968, "north_suburbs", "/images/reccomendations/rebecca-crown-center.jpg", TRUE),
+    -- Test to make sure that I am indeed using AWS and not the local database
+    ("PLACEHOLDER NAME", "AWS online hosting", "Internet", 2006, "Amazon website", NULL, TRUE);
