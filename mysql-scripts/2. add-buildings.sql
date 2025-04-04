8c1e42cbd2648c797a994ea8932512c1c5db4509
Jacob <ll1bytecode@gmail.com>
2025-04-04 08:12:48 -0500

    Add images for North Side - River North and Streeterville.

--- mysql-scripts/2. add-buildings.sql
+++ mysql-scripts/2. add-buildings.sql
@@ -77,32 +77,32 @@ VALUES
 -- North Side: River North
 INSERT INTO chicagoarchitecture.building (name, address, city, creation_year, district, image_path)
 VALUES 
-    ("Trump International Hotel & Tower", "401 North Wabash Avenue", "Chicago", "2009", "river_north", NULL),
-    ("Museum of Broadcast Communications", "360 North State Street", "Chicago", "2012", "river_north", NULL),
-    ("300 North LaSalle", "300 North LaSalle Street", "Chicago", "2009", "river_north", NULL),
-    ("Wolf Point", "343 West Wold Point Plaza", "Chicago", "2023", "river_north", NULL),
-    ("Contemporaine", "516 North WElls Street", "Chicago", "2004", "river_north", NULL),
-    ("McDonald's Chicago Flagship", "600 N. Clark Street", "Chicago", "2018", "river_north", NULL),
-    ("Erie on the Park", "510 West Erie Street", "Chicago", "2002", "river_north", NULL),
-    ("156 West Superior", "156 West Superior Street", "Chicago", "2006", "river_north", NULL),
-    ("The Godfrey Hotel", "127 West Huron Street", "Chicago", "2014", "river_north", NULL),
-    ("Poetry Foundation", "61 West Superior Street", "Chicago", "2011", "river_north", NULL);
+    ("Trump International Hotel & Tower", "401 North Wabash Avenue", "Chicago", "2009", "river_north", "/images/north-side/trump-international-tower.jpg"),
+    ("Museum of Broadcast Communications", "360 North State Street", "Chicago", "2012", "river_north", "/images/north-side/museum-of-broadcast-communications.jpg"),
+    ("300 North LaSalle", "300 North LaSalle Street", "Chicago", "2009", "river_north", "/images/north-side/300-north-lasalle.jpg"),
+    ("Wolf Point", "343 West Wold Point Plaza", "Chicago", "2023", "river_north", "/images/north-side/wolf-point-west.jpg"),
+    ("Contemporaine", "516 North WElls Street", "Chicago", "2004", "river_north", "/images/north-side/contemporaine.jpg"),
+    ("McDonald's Chicago Flagship", "600 N. Clark Street", "Chicago", "2018", "river_north", "/images/north-side/mcdonalds-chicago-flagship.jpg"),
+    ("Erie on the Park", "510 West Erie Street", "Chicago", "2002", "river_north", "/images/north-side/erie-on-the-park.jpeg"),
+    ("156 West Superior", "156 West Superior Street", "Chicago", "2006", "river_north", "/images/north-side/156-west-superior.jpg"),
+    ("The Godfrey Hotel", "127 West Huron Street", "Chicago", "2014", "river_north", "/images/north-side/godfrey-hotel.jpg"),
+    ("Poetry Foundation", "61 West Superior Street", "Chicago", "2011", "river_north", "/images/north-side/poetry-foundation.jpg");
 
 
 -- North Side: Streeterville
 INSERT INTO chicagoarchitecture.building (name, address, city, creation_year, district, image_path)
 VALUES 
-    ("Apple Michigan Avenue", "401 North Michigan Avenue", "Chicago", "2017", "streeterville", NULL),
-    ("Optima Chicago Center", "200 East Illinois Street", "Chicago", "2013", "streeterville", NULL),
-    ("600 North Fairbanks", "600 North Fairbanks Court", "Chicago", "2007", "streeterville", NULL),
-    ("One Bennett Park", "451 East Grand Avenue", "Chicago", "2019", "streeterville", NULL),
-    ("Navy Pier Pierscape", "600 East Grand Avenue", "Chicago", "2016", "streeterville", NULL),
-    ("The Yard at Chicago Shakespeare", "800 East Grand Avenue", "Chicago", "2017", "streeterville", NULL),
-    ("Simpson Querrey Biomedical Research Center", "303 East Superior Street", "Chicago", "2019", "streeterville", NULL),
-    ("Lurie Children's Hospital", "225 East Chicago Avenue", "Chicago", "2012", "streeterville", NULL),
-    ("Park Tower", "800 North Michigan Avenue", "Chicago", "2000", "streeterville", NULL),
-    ("Sofitel Hotel", "20 East Chestnut Street", "Chicago", "2002", "streeterville", NULL),
-    ("Fourth Presbyterian Church of Chicago", "126 East Chestnut Street", "Chicago", "2013", "streeterville", NULL);
+    ("Apple Michigan Avenue", "401 North Michigan Avenue", "Chicago", "2017", "streeterville", "/images/north-side/apple-michigan-avenue.jpg"),
+    ("Optima Chicago Center", "200 East Illinois Street", "Chicago", "2013", "streeterville", "/images/north-side/optima-center.jpg"),
+    ("600 North Fairbanks", "600 North Fairbanks Court", "Chicago", "2007", "streeterville", "/images/north-side/600-north-fairbanks.jpg"),
+    ("One Bennett Park", "451 East Grand Avenue", "Chicago", "2019", "streeterville", "/images/north-side/one-bennett-park.jpg"),
+    ("Navy Pier Pierscape", "600 East Grand Avenue", "Chicago", "2016", "streeterville", "/images/north-side/navy-pier-pierscape.jpg"),
+    ("The Yard at Chicago Shakespeare", "800 East Grand Avenue", "Chicago", "2017", "streeterville", "/images/north-side/chicago-shakespeare-theater.jpg"),
+    ("Simpson Querrey Biomedical Research Center", "303 East Superior Street", "Chicago", "2019", "streeterville", "/images/north-side/simpson-querrey-biomedical-research-center.jpg"),
+    ("Lurie Children's Hospital", "225 East Chicago Avenue", "Chicago", "2012", "streeterville", "/images/north-side/lurie-childrens-hospital.jpg"),
+    ("Park Tower", "800 North Michigan Avenue", "Chicago", "2000", "streeterville", "/images/north-side/park-tower.jpg"),
+    ("Sofitel Hotel", "20 East Chestnut Street", "Chicago", "2002", "streeterville", "/images/north-side/sofitel-hotel-chicago.jpg"),
+    ("Fourth Presbyterian Church of Chicago", "126 East Chestnut Street", "Chicago", "2013", "streeterville", "/images/north-side/fourth-presbyterian-church.jpg");
     
 
 -- North Side: Near North Side
