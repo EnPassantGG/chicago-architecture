78d091f43147aff167a250a1ad1abc09afecba44
Jacob <ll1bytecode@gmail.com>
2025-04-08 17:22:02 -0500

    Add "About" page and corresponding controller mapping.

--- src/main/java/com/example/chicagoarchitecture/controller/HomepageController.java
+++ src/main/java/com/example/chicagoarchitecture/controller/HomepageController.java
@@ -66,4 +66,9 @@ public class HomepageController {
         return "bootstrap-demo";
     }
 
+    @GetMapping("/bootstrap-about")
+    public String bootstrapAbout() {
+        return "bootstrap-about";
+    }
+
 }
